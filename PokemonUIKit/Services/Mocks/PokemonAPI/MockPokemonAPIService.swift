914a3f5f8ef17d36949620b35bb50093fa53e49d
Eduardo Cordeiro da Camara <eduardo.camara@icomp.ufam.edu.br>
2025-06-27 19:08:42 -0400

    fix: correct methods signatures in MockPokemonAPIService

--- PokemonUIKit/Services/Mocks/PokemonAPI/MockPokemonAPIService.swift
+++ PokemonUIKit/Services/Mocks/PokemonAPI/MockPokemonAPIService.swift
@@ -8,11 +8,11 @@
 import Foundation
 
 final class MockPokemonAPIService: PokemonAPIServiceProtocol {
-    func fetchPokemonList(completion: @escaping (Result<[PokemonModel], any Error>) -> Void) {
+    func fetchPokemonList(offset: Int, limit: Int, completion: @escaping (Result<[PokemonModel], any Error>) -> Void) {
         
     }
     
-    func fetchPokemonDetail(from url: URL, completion: @escaping (Result<PokemonDetailsModel, any Error>) -> Void) {
+    func fetchPokemonDetails(ofId id: Int, completion: @escaping (Result<PokemonDetailsModel, any Error>) -> Void) {
         
     }
 }
