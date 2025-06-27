9810092239da8c0854b2eaad0f1896ae485602ed
Eduardo Cordeiro da Camara <eduardo.camara@icomp.ufam.edu.br>
2025-06-27 12:23:43 -0400

    feat: implement PokemonService with stubbed fetch methods

--- PokemonUIKit/Services/PokemonAPI/PokemonAPIService.swift
+++ PokemonUIKit/Services/PokemonAPI/PokemonAPIService.swift
@@ -5,3 +5,37 @@
 //  Created by Eduardo Cordeiro da Camara on 27/06/25.
 //
 
+import Foundation
+
+final class PokemonService: PokemonAPIServiceProtocol {
+    let networkClient: NetworkClientProtocol
+
+    init(networkClient: NetworkClientProtocol = NetworkClient()) {
+        self.networkClient = networkClient
+    }
+    
+    func fetchPokemonList(completion: @escaping (Result<[Pokemon], Error>) -> Void) {
+        let urlString = "https://pokeapi.co/api/v2/pokemon?limit=151"
+        
+//        networkClient.fetch(from: urlString, decodeTo: PokemonListResponse.self) { result in
+//            switch result {
+//            case .success(let response):
+//                let pokemons = response.results.map { $0.toDomainModel() }
+//                completion(.success(pokemons))
+//            case .failure(let error):
+//                completion(.failure(error))
+//            }
+//        }
+    }
+    
+    func fetchPokemonDetail(from url: URL, completion: @escaping (Result<PokemonDetail, Error>) -> Void) {
+//        networkClient.fetch(from: url.absoluteString, decodeTo: PokemonDetailResponse.self) { result in
+//              switch result {
+//              case .success(let response):
+//                  completion(.success(response.toDomainModel()))
+//              case .failure(let error):
+//                  completion(.failure(error))
+//              }
+//          }
+      }
+}
