338918624565282f0d75d7615b5f6da923d0ab63
Eduardo Cordeiro da Camara <eduardo.camara@icomp.ufam.edu.br>
2025-06-27 14:34:40 -0400

    chore: add PokemonListResponse as API Model

---
+++ PokemonUIKit/Models/Pokemon/API/PokemonListResponse.swift
@@ -0,0 +1,12 @@
+//
+//  PokemonListResponse.swift
+//  PokemonUIKit
+//
+//  Created by Eduardo Cordeiro da Camara on 27/06/25.
+//
+
+import Foundation
+
+struct PokemonListResponse: Decodable {
+    
+}
