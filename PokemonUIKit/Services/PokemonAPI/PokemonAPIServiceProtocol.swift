1a1077465dcc9b3fd0bc1fc113d8de4592d12d7d
Eduardo Cordeiro da Camara <eduardo.camara@icomp.ufam.edu.br>
2025-06-27 16:49:19 -0400

    refactor: change fetchPokemonList signature to have offset and limit as parameters

--- PokemonUIKit/Services/PokemonAPI/PokemonAPIServiceProtocol.swift
+++ PokemonUIKit/Services/PokemonAPI/PokemonAPIServiceProtocol.swift
@@ -8,6 +8,6 @@
 import Foundation
 
 protocol PokemonAPIServiceProtocol {
-    func fetchPokemonList(completion: @escaping (Result<[PokemonModel], Error>) -> Void)
+    func fetchPokemonList(offset: Int, limit: Int, completion: @escaping (Result<[PokemonModel], Error>) -> Void)
     func fetchPokemonDetail(from url: URL, completion: @escaping (Result<PokemonDetailsModel, Error>) -> Void)
 }
