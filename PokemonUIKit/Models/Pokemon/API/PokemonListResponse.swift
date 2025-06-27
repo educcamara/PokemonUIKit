2d47a75c4147635305d2e5f17084e5b8afc506a7
Eduardo Cordeiro da Camara <eduardo.camara@icomp.ufam.edu.br>
2025-06-27 16:22:52 -0400

    feat: create toDomainModel function to PokemonListItemResponse with its helper methods

--- PokemonUIKit/Models/Pokemon/API/PokemonListResponse.swift
+++ PokemonUIKit/Models/Pokemon/API/PokemonListResponse.swift
@@ -22,3 +22,35 @@ struct PokemonListItemResponse: Decodable {
         case urlString = "url"
     }
 }
+
+//MARK: Reponse to DomainModel
+extension PokemonListItemResponse {
+    func toDomainModel() -> PokemonModel {
+        let id: Int = extractPokemonId(from: self.urlString) ?? 0
+        let imageUrl = URL(string: imageUrlString(with: id))
+        let url = URL(string: self.urlString)
+        
+        return PokemonModel(
+            id: id,
+            name: self.name,
+            imageUrl: imageUrl,
+            url: url)
+    }
+    
+    /// Extracts the Pokemon ID from a PokeAPI URL string.
+    /// - Parameter urlString: The API URL (e.g., "https://pokeapi.co/api/v2/pokemon/25/")
+    /// - Returns: The Pokemon ID as an integer, or nil if extraction fails
+    private func extractPokemonId(from urlString: String) -> Int? {
+       let components = urlString.split(separator: "/")
+       guard let last = components.last else { return nil }
+       if last.isEmpty {
+           return Int(components[components.count - 2])
+       } else {
+           return Int(last)
+       }
+    }
+    
+    private func imageUrlString(with id: Int) -> String {
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/\(id).png"
+    }
+}
