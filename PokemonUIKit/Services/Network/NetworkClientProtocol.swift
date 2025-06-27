3383d222cfc868e245ef862467b4f3032ecc6fc6
Eduardo Cordeiro da Camara <eduardo.camara@icomp.ufam.edu.br>
2025-06-27 12:03:12 -0400

    chore: add NetworkClientProtocol

---
+++ PokemonUIKit/Services/Network/NetworkClientProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkClientProtocol.swift
+//  PokemonUIKit
+//
+//  Created by Eduardo Cordeiro da Camara on 27/06/25.
+//
+
+protocol NetworkClientProtocol {
+    func fetch<T: Decodable>(
+        from urlString: String,
+        decodeTo type: T.Type,
+        completion: @escaping (Result<T, Error>) -> Void)
+}
