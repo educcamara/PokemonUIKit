c4ded972ff0434e809d24b45a5814a60e9b84df9
Eduardo Cordeiro da Camara <eduardo.camara@icomp.ufam.edu.br>
2025-06-27 19:02:23 -0400

    refactor: simplify MockNetworkClient with Response enum and config-based init

--- PokemonUIKit/Services/Mocks/Network/MockNetworkClient.swift
+++ PokemonUIKit/Services/Mocks/Network/MockNetworkClient.swift
@@ -8,72 +8,60 @@
 import Foundation
 
 final class MockNetworkClient: NetworkClientProtocol {
-   
-   // MARK: - Properties
-   var shouldReturnError = false
-   var errorToReturn: NetworkError = .invalidURL
-   var dataToReturn: Data?
-   var responseDelay: TimeInterval = 0.0
-   
-   // MARK: - NetworkClientProtocol Implementation
-   func fetch<T: Decodable>(
-       from urlString: String,
-       decodeTo type: T.Type,
-       completion: @escaping (Result<T, NetworkError>) -> Void
-   ) {
-       // Simulate network delay if specified
-       DispatchQueue.global().asyncAfter(deadline: .now() + responseDelay) {
-           if self.shouldReturnError {
-               completion(.failure(self.errorToReturn))
-               return
-           }
-           
-           guard let data = self.dataToReturn else {
-               completion(.failure(.emptyData))
-               return
-           }
-           
-           do {
-               let decoder = JSONDecoder()
-               decoder.keyDecodingStrategy = .convertFromSnakeCase
-               let decoded = try decoder.decode(type, from: data)
-               completion(.success(decoded))
-           } catch {
-               completion(.failure(.custom(error)))
-           }
-       }
-   }
+    
+    // MARK: - Properties
+    private var delay: TimeInterval
+    private var response: Response
+    
+    enum Response {
+        case success(data: Data)
+        case failure(error: Error)
+    }
+    
+    //MARK: - Init
+    init(
+        delay: TimeInterval = 0,
+        configuration: Response,
+    ) {
+        self.delay = delay
+        self.response = configuration
+    }
+    
+    
+    // MARK: - NetworkClientProtocol Implementation
+    func fetch<T: Decodable>(
+        from urlString: String,
+        decodeTo type: T.Type,
+        completion: @escaping (Result<T, NetworkError>) -> Void
+    ) {
+        DispatchQueue.global().asyncAfter(deadline: .now() + delay) { [weak self] in
+            guard let self else { return }
+            
+            switch self.response {
+            case .success(let data):
+                do {
+                    let decoder = JSONDecoder()
+                    decoder.keyDecodingStrategy = .convertFromSnakeCase
+                    let decoded = try decoder.decode(type, from: data)
+                    completion(.success(decoded))
+                } catch {
+                    completion(.failure(.custom(error)))
+                }
+            case .failure(let error):
+                if let networkError = error as? NetworkError {
+                    completion(.failure(networkError))
+                } else {
+                    completion(.failure(.custom(error)))
+                }
+            }
+        }
+    }
 }
 
-// MARK: - Test Helpers
+// MARK: - Helpers
 extension MockNetworkClient {
-   
-   /// Sets up mock to return successful response with provided data
-   func setupSuccessResponse<T: Encodable>(with object: T, delay: TimeInterval = 0.0) {
-       let encoder = JSONEncoder()
-       encoder.keyEncodingStrategy = .convertToSnakeCase
-       
-       do {
-           self.dataToReturn = try encoder.encode(object)
-           self.shouldReturnError = false
-           self.responseDelay = delay
-       } catch {
-           print("Failed to encode mock data: \(error)")
-       }
-   }
-   
-   /// Sets up mock to return error response
-   func setupErrorResponse(error: NetworkError, delay: TimeInterval = 0.0) {
-       self.shouldReturnError = true
-       self.errorToReturn = error
-       self.responseDelay = delay
-   }
-   
-   /// Resets mock to initial state
-   func reset() {
-       shouldReturnError = false
-       errorToReturn = .invalidURL
-       dataToReturn = nil
-       responseDelay = 0.0
-   }
+    func setupNewResponse(_ response: Response, delay: TimeInterval = 0) {
+        self.delay = delay
+        self.response = response
+    }
 }
